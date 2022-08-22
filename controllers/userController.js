93d65306f9f6de2025338788583b4001b3afc1b2
greewank <gkc1s@semo.edu>
2022-08-22 17:12:55 -0400

    Added user data controlling functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,14 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/userModel');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 // The users resource
 exports.getAllUsers = (req, res) => {
   //500 is internal server error
@@ -6,6 +17,32 @@ exports.getAllUsers = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create an error if  user POSTS(updates) password data.
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use the /updateMyPassword route!',
+        400
+      )
+    );
+  }
+  // 2. Filtered out field names that are not to be updated.
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // 3.Update user document.
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.getUser = (req, res) => {
   //500 is internal server error
